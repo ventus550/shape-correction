08a5b18289b2766a1c4f81f350008c83b2726024
Ventus <ventuss550@gmail.com>
2023-03-05 20:13:47 +0100

    feat: skeletonize input

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
 from PIL import Image, ImageOps
+import numpy as np
 from numpy import argmax, nonzero
+from skimage.morphology import skeletonize
+import cv2
 
 # silence tf warnings
 import os; os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -11,6 +14,17 @@ def transform(image: Image):
 
 def img2tensor(image: Image):
 	image = tf.keras.preprocessing.image.img_to_array(image) #/ 255.0
+	image[nonzero(image)] = 1.0
+	image = skeletonize(image)
+	image = cv2.dilate(
+		image,
+		cv2.getStructuringElement(cv2.MORPH_RECT, (2,2)),
+		iterations=1
+	)
+
+	# kernel2 = np.ones((2, 2), np.float32)/4
+	# image = cv2.filter2D(src=image, ddepth=-1, kernel=kernel2)
+	
 	image[nonzero(image)] = 1.0
 	return tf.constant((image,))
 
