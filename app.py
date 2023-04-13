bef12e39ee71578d6c1ff5f9acdad3a3d1b599ab
Ventus <ventuss550@gmail.com>
2023-04-13 17:26:11 +0200

    refactor: black

--- app.py
+++ app.py
@@ -3,79 +3,80 @@ from TkCanvas.canvas import Canvas
 from models import Classifier, Regressor
 from preprocessing import preprocess
 
+
 class DrawingCanvas(Canvas):
-	def __init__(self, width=1000, height=1000):
-		super().__init__(width, height)
-		self.points = []
-		self.register_mouse_press(self.on_click)
-		self.register_mouse_move(self.on_move)
-		self.register_mouse_release(self.on_release)
-		self.classifer = Classifier("models/classifier")
-		self.regressors = {
-			"rectangle": Regressor("models/rectangle_regressor"),
-			"ellipse": Regressor("models/ellipse_regressor"),
-			"triangle": Regressor("models/triangle_regressor")
-		}
+    def __init__(self, width=1000, height=1000):
+        super().__init__(width, height)
+        self.points = []
+        self.register_mouse_press(self.on_click)
+        self.register_mouse_move(self.on_move)
+        self.register_mouse_release(self.on_release)
+        self.classifer = Classifier("models/classifier")
+        self.regressors = {
+            "rectangle": Regressor("models/rectangle_regressor"),
+            "ellipse": Regressor("models/ellipse_regressor"),
+            "triangle": Regressor("models/triangle_regressor"),
+        }
 
-	def draw_vertices(self, vertices: list[float]):
-		self.stroke_color = 'red'
-		for v in vertices:
-			self.point(*v)
+    def draw_vertices(self, vertices: list[float]):
+        self.stroke_color = "red"
+        for v in vertices:
+            self.point(*v)
 
-	def connect(self, vertices):
-		centroid = sum(vertices) / len(vertices)
-		angle = lambda v: np.arctan2(*(v - centroid))
-		vertices = sorted(vertices, key=angle)
-		
-		for v, u in zip(vertices, vertices[1:]):
-			self.line(*v, *u)
-		self.line(*vertices[0], *u)
+    def connect(self, vertices):
+        centroid = sum(vertices) / len(vertices)
+        angle = lambda v: np.arctan2(*(v - centroid))
+        vertices = sorted(vertices, key=angle)
 
-	def ellipse(self, vertices):
-		centroid = sum(vertices) / len(vertices)
-		angle = lambda v: np.arctan2(*(v - centroid))
-		vertices = sorted(vertices, key=angle)
-		
-		for v, u in zip(vertices, vertices[1:]):
-			x = v + u - centroid
-			self.curve([v, x, u])
-		self.curve([vertices[0], vertices[0] + u - centroid, u])
+        for v, u in zip(vertices, vertices[1:]):
+            self.line(*v, *u)
+        self.line(*vertices[0], *u)
 
-	def reconstruct(self, vertices, shape):
-		self.reset()
-		if shape == "other":
-			return
-		elif shape == "ellipse":
-			self.ellipse(vertices)
-		else:
-			self.connect(vertices)
+    def ellipse(self, vertices):
+        centroid = sum(vertices) / len(vertices)
+        angle = lambda v: np.arctan2(*(v - centroid))
+        vertices = sorted(vertices, key=angle)
 
-	def on_click(self, _):
-		self.reset()
-		self.points.clear()
+        for v, u in zip(vertices, vertices[1:]):
+            x = v + u - centroid
+            self.curve([v, x, u])
+        self.curve([vertices[0], vertices[0] + u - centroid, u])
 
-	def on_move(self, e):
-		self.reset()
-		self.points.append((e.x, e.y))
-		self.stroke_color = "black"
-		self.curve(self.points)
+    def reconstruct(self, vertices, shape):
+        self.reset()
+        if shape == "other":
+            return
+        elif shape == "ellipse":
+            self.ellipse(vertices)
+        else:
+            self.connect(vertices)
 
-	def on_release(self, _):
-		img, xy = self.capture()
-		pimg = preprocess(img)
+    def on_click(self, _):
+        self.reset()
+        self.points.clear()
 
-		shape = self.classifer.classify(pimg)
-		print(shape)
-		if shape == "other": return
-		
-		vertices = self.regressors[shape].vertices(pimg)
-		vertices = np.array(vertices).reshape((len(vertices)//2, 2))
-		vertices *= img.size
-		vertices += xy
+    def on_move(self, e):
+        self.reset()
+        self.points.append((e.x, e.y))
+        self.stroke_color = "black"
+        self.curve(self.points)
 
-		self.draw_vertices(vertices)
-		self.root.after(500, lambda: self.reconstruct(vertices, shape))
+    def on_release(self, _):
+        img, xy = self.capture()
+        pimg = preprocess(img)
 
+        shape = self.classifer.classify(pimg)
+        print(shape)
+        if shape == "other":
+            return
+
+        vertices = self.regressors[shape].vertices(pimg)
+        vertices = np.array(vertices).reshape((len(vertices) // 2, 2))
+        vertices *= img.size
+        vertices += xy
+
+        self.draw_vertices(vertices)
+        self.root.after(500, lambda: self.reconstruct(vertices, shape))
 
 
 canvas = DrawingCanvas()
--- models.py
+++ models.py
@@ -1,26 +1,29 @@
-# silence tf warnings
-import os; os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import os
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import tensorflow as tf
 from numpy import argmax, ndarray
 
 
 class ModelInterface:
-	def __init__(self, path):
-		self.model = tf.keras.models.load_model(path, compile=False)
-		self.model.compile()
+    def __init__(self, path):
+        self.model = tf.keras.models.load_model(path, compile=False)
+        self.model.compile()
 
-	@staticmethod
-	def img2tensor(image: ndarray):
-		return tf.constant((image, ))
+    @staticmethod
+    def img2tensor(image: ndarray):
+        return tf.constant((image,))
+
 
 class Classifier(ModelInterface):
-	def classify(self, image: ndarray):
-		shapes = ['other', 'ellipse', 'rectangle', 'triangle']
-		image_tensor = self.img2tensor(image)
-		dist = self.model(image_tensor)
-		return shapes[argmax(dist)]
+    def classify(self, image: ndarray):
+        shapes = ["other", "ellipse", "rectangle", "triangle"]
+        image_tensor = self.img2tensor(image)
+        dist = self.model(image_tensor)
+        return shapes[argmax(dist)]
+
 
 class Regressor(ModelInterface):
-	def vertices(self, image: ndarray):
-		image_tensor = self.img2tensor(image)
-		return self.model(image_tensor)[0]
+    def vertices(self, image: ndarray):
+        image_tensor = self.img2tensor(image)
+        return self.model(image_tensor)[0]
